9af1f0f12915c10c0a4e06625f46bba8ba73d6da
bahirathy <bahi.rathy@rapidqube.com>
2018-06-21 15:44:46 +0530

    change

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,9 +3,8 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     // model(){
         
-        //this.transitionTo('govern')
+    // this.transitionTo('main')
         
 
-         
-     //}
+    // }
 });
